90d1dff738423edd2afb8f06748acd25b04fd2fe
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-19 00:12:31 +0300

    feat: add basic endpoints

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,23 @@ app.get("/", (req, res) => {
   res.json("Hello World from the Server")
 })
 
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books"
+  pool.query(q, (err, result) => {
+    if (err) return res.json(err.message)
+    return res.json(result)
+  })
+})
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover_img`,`price`) VALUES (?)"
+  const values = ["BookServer", "DescServer", "imageserver.png", "45"]
+  pool.query(q, [values], (err, result) => {
+    if (err) return res.json(err)
+    return console.log("POST success from book server")
+  })
+})
+
 // Run Server
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`)
