6fcd1b16a03c549dd3495c22dab785a3c55f5d7b
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-19 00:11:38 +0300

    feat: connect bookshop db with the Server

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,21 @@
 const express = require("express")
+const mysql = require("mysql2")
 
 const app = express()
 const port = 8800
 
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "bookshop",
+  password: "h1ga4feP",
+})
+
+app.get("/", (req, res) => {
+  res.json("Hello World from the Server")
+})
+
+// Run Server
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`)
 })
