78da2e25b1c6aef32afc79f0e66e294e856027fe
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-19 23:58:38 +0300

    feat: connect main page with the Server

--- Client/src/pages/Books.jsx
+++ Client/src/pages/Books.jsx
@@ -1,7 +1,38 @@
 import React from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const Books = () => {
-  return <div>Books</div>
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books")
+        setBooks(res.data)
+      } catch (err) {
+        console.log(`Error fetching All books ${err.message}`)
+      }
+    }
+    fetchAllBooks()
+  }, [])
+
+  return (
+    <div>
+      <h1>BookShop where you can Explore the best Sellers</h1>
+      {books.map((book) => {
+        return (
+          <div key={book.id}>
+            <h2>{book.title}</h2>
+            <h2>{book.desc}</h2>
+            <h2>{book.cover_img}</h2>
+            <h3>{book.price}</h3>
+            <h5>--------------</h5>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Books
