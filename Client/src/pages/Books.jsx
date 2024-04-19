46a25641fbe3ebb55d3edfff4b4fcf76fe0af7d0
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-20 02:06:47 +0300

    feat: add button to Navigate to Add component

--- Client/src/pages/Books.jsx
+++ Client/src/pages/Books.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { Link } from "react-router-dom"
 
 const Books = () => {
   const [books, setBooks] = useState([])
@@ -25,12 +26,17 @@ const Books = () => {
           <div key={book.id}>
             <h2>{book.title}</h2>
             <h2>{book.desc}</h2>
-            <h2>{book.cover_img}</h2>
+            <div>
+              {book.cover_img && <img src={book.cover_img} alt={book.title} />}
+            </div>
             <h3>{book.price}</h3>
             <h5>--------------</h5>
           </div>
         )
       })}
+      <button>
+        <Link to='/add'>Add New Book</Link>
+      </button>
     </div>
   )
 }
