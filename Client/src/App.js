1a66030732dd3371bfd4d4a8e75e8bd0d84622d0
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-04-22 01:31:35 +0300

    docs: add classname as root

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,10 +3,11 @@ import "./App.css"
 import Books from "./pages/Books"
 import Add from "./pages/Add"
 import Update from "./pages/Update"
+import "./style.css"
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Books />} />
